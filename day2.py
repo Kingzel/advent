506d592981f7310f48cfddfa82aade69b696cfcb
dudeishan <styxnerthegamer@gmail.com>
2023-12-05 02:34:18 +0800

    part 2 finished

--- day2.py
+++ day2.py
@@ -4,26 +4,25 @@ with open("2.txt") as f:
 
 acc = 0
 games = []
-#12 red cubes, 13 green cubes, and 14 blue cubes
-maxallowed = {'red':12,'green':13,'blue':14}
 for i in range(len(lines)):
     game = lines[i].split(';',-1)
     game[0]=game[0].split(':')[1]
     games.append(game)
 
 for i in range(len(games)):
-    b = True
+    maxallowed = {'red':0,'green':0,'blue':0}
     for subset in games[i]:
         colors = subset.split(',',-1)
         for color in colors:
             count, key = color.split()
-            if int(count)>maxallowed[key]:
-                b = False
-    ti = i+1
-    # print( ti,":", b)
+            maxallowed[key] = max(maxallowed[key],int(count))
+    
+    t = 1
+    for elem in list(maxallowed.values()):
+        t*=elem
+    print(maxallowed.values())
+    print(games[i],t)
+    acc +=t
 
-    if b:
-        acc += ti
 
-# print(subsets)
 print(acc)
