b812728993f3e63d70d1e026e261bf7070e70b2e
dudeishan <styxnerthegamer@gmail.com>
2023-12-10 00:54:11 +0530

    cleaned code for day 9

--- day9b.py
+++ day9b.py
@@ -7,19 +7,17 @@ def genInvPyramid(diffs,collection):
     toapp = copy.copy(diffs)
     collection.append(toapp)
     if all(diff == 0 for diff in diffs):
-        del diffs
         collection[-1].append(0)
         return collection
     else:
         for i in range(len(diffs)-1):
-            diffs[i]=(l[i+1]-l[i])
+            diffs[i]=(diffs[i+1]-diffs[i])
         diffs.pop()
         return genInvPyramid(diffs,collection)
+
 fullsum = 0
 for line in lines:
-    l =[int(el) for el in  line.split()]
-    output = genInvPyramid(l,[])
-    listsum =0
+    output,listsum = genInvPyramid([int(el) for el in  line.split()],[]),0
     for i in range(len(output)-1,-1,-1):
         if i > 0:
             level = output[i]
